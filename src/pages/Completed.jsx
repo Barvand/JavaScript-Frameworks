666c3b456f843478e37e705d8b3389ee9115c484
B <128028346+Barvand@users.noreply.github.com>
2024-12-19 21:23:20 +0100

    update

--- src/pages/Completed.jsx
+++ src/pages/Completed.jsx
@@ -5,7 +5,7 @@ export default function CompletedOrder() {
   return (
     <div className="flex flex-col items-center gap-5 ">
       <AiFillCheckCircle className="text-5xl text-green-500" />
-      <h1 className="text-green-500 text-4xl">Form submitted successfully</h1>
+      <h1 className="text-green-500 text-4xl">Order submitted successfully</h1>
       <p>
         We have received your order, you will receive an order confirmation
         shortly.
