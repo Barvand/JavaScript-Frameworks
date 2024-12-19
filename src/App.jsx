3b0cf8cd7a0e1bc0a6561b1adc32d725fe07317e
B <128028346+Barvand@users.noreply.github.com>
2024-12-19 20:04:12 +0100

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useLocation } from "react-router-dom";
 import { Nav } from "./components/nav.jsx";
 import { ProductsPage } from "./pages/Product.jsx";
 import { ContactPage } from "./pages/ContactPage.jsx"; 
-import { Home } from "./pages/Home.jsx";
+import { Home } from "./pages/HomePage.jsx";
 import { Route, Routes } from "react-router-dom";
 import { ProductPage } from "./pages/ProductPage.jsx";
 import { ShoppingCart } from "./pages/ShoppingCart.jsx";
