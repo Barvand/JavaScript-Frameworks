463dda8bd6d9ea10667fce54ff9cea4de48fe66a
B <128028346+Barvand@users.noreply.github.com>
2024-12-19 20:07:39 +0100

    fix maybe?

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { useLocation } from "react-router-dom";
 import { Nav } from "./components/nav.jsx";
-import { ProductsPage } from "./pages/Product.jsx";
+import { ProductsPage } from "./pages/ProductCards.jsx";
 import { ContactPage } from "./pages/ContactPage.jsx"; 
 import { Home } from "./pages/HomePage.jsx";
 import { Route, Routes } from "react-router-dom";
 import { ProductPage } from "./pages/ProductPage.jsx";
-import { ShoppingCart } from "./pages/ShoppingCart.jsx";
+import { ShoppingCart } from "./pages/CartPage.jsx";
 import SideCartMenu from "./components/CartSideMenu.jsx";
 import Footer from "./components/Footer.jsx";
-import SuccessPage from "./pages/Success.jsx";
+import SuccessPage from "./pages/SuccessPage.jsx";
 import CompletedOrder from "./pages/Completed.jsx";
 
 const hideMenuOnCartPage = ["/cart"]; // Define routes where the menu is hidden
