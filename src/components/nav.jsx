05702f78d1283f0c07b4c44a70353ae58d81441b
B <128028346+Barvand@users.noreply.github.com>
2024-12-19 21:00:31 +0100

    added hamburger menu

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -2,28 +2,12 @@ import { Link } from "react-router-dom";
 import Logo from "../svg/logo";
 import { useCartStore } from "../store/cart";
 import Hamburger from "hamburger-react";
-import { useState, useRef, useEffect } from "react";
-import SearchBar from "./searchbar,";
-
+import { useState } from "react";
+import SearchBar from "./Searchbar,";
 
 export function Navigation() {
   const { carts } = useCartStore();
-  const menuRef = useRef(null);
   const [isOpen, setOpen] = useState(false);
-
-  useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (menuRef.current && !menuRef.current.contains(event.target)) {
-        setOpen(false);
-      }
-    };
-
-    document.addEventListener("mousedown", handleClickOutside);
-
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, []);
 
   return (
     <nav className="sm:container p-2 font-koulen text-xl gradient-border-bottom-yellow pb-5">
@@ -35,10 +19,10 @@ export function Navigation() {
             </Link>
           </li>
           <li className="hidden md:block">
-            <Link to="/contact">Contact</Link>
+            <Link to="/products">Products</Link>
           </li>
           <li className="hidden md:block">
-            <Link to="/products">Products</Link>
+            <Link to="/contact">Contact</Link>
           </li>
           <Link to="/cart" className="nav--shopping-cart">
             <i className="fa-solid fa-cart-shopping"></i>
@@ -47,25 +31,58 @@ export function Navigation() {
             </div>
           </Link>
           <>
-            <div className="block md:hidden z-40" ref={menuRef}>
-              <Hamburger toggled={isOpen} toggle={setOpen} />
+            <div className="block md:hidden">
+              <Hamburger
+                toggled={isOpen}
+                toggle={setOpen}
+                aria-expanded={isOpen}
+                aria-label="Toggle navigation"
+              />
             </div>
             <div
               className={`hamburger-width gradient-border z-30 transform transition-transform duration-500 ease-in-out ${
                 isOpen ? "translate-x-0" : "translate-x-full"
               }`}
             >
-              <nav className="sm:container p-4 font-koulen text-xl items-center">
-                <div className="md:container flex flex-col items-center justify-center h-full text-center">
-                  <Link to="/" className="mb-4">
+              <nav className="sm:container p-4 font-koulen text-xl flex justify-center gap-5 relative h-dvh">
+                <div className="md:container flex flex-col gap-5">
+                  <div className="absolute right-5 top-10">
+                    <Hamburger
+                      toggled={isOpen}
+                      toggle={setOpen}
+                      aria-expanded={isOpen}
+                      aria-label="Toggle navigation"
+                    />
+                  </div>
+                  <Link to="/" className="mb-4" onClick={() => setOpen(false)}>
                     <Logo />
                   </Link>
-                  <ul className="flex flex-col gap-3 text-center">
+                  <ul className="flex flex-col gap-5 text-center">
                     <li>
-                      <Link to="/products">Products</Link>
+                      <Link to="/products" onClick={() => setOpen(false)}>
+                        {" "}
+                        Products{" "}
+                      </Link>
                     </li>
                     <li>
-                      <Link to="/contact">Contact</Link>
+                      <Link to="/contact" onClick={() => setOpen(false)}>
+                        Contact
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="/cart"
+                        className="nav--shopping-cart"
+                        onClick={() => setOpen(false)}
+                      >
+                        <i className="fa-solid fa-cart-shopping"></i>
+                        <div className="nav--shopping-cart-count">
+                          {carts.reduce(
+                            (total, item) => total + item.quantity,
+                            0
+                          )}
+                        </div>
+                      </Link>
                     </li>
                   </ul>
                 </div>
